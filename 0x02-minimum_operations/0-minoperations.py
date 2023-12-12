eadb6614502a6501067debe98bf86d8703830179
IdiJr <idialijnr@gmail.com>
2023-12-12 10:27:28 +0000

    Minimum Operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Calculates the fewest number of operations needed
+to result in exactly n H characters in a file.
+"""
+
+
+def minOperations(n):
+    """ Description: Takes in a given number n and calculates
+                     the minimum operations needed. """
+    if n <= 1:
+        return 0
+
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return i + minOperations(n // i)
+
+    return n
