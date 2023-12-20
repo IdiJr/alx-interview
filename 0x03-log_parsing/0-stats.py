811b3d516707a86c4ba852cf04d51a600e61d4e5
IdiJr <idialijnr@gmail.com>
2023-12-20 13:34:25 +0000

    Log Parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""reads the stdin line by line and computes metrices"""
+"""reads the stdin line by line and computes metrics"""
 
 import sys
 
@@ -10,43 +10,40 @@ def print_stats(total_size, status_codes):
         print("{}: {}".format(code, status_codes[code]))
 
 
-def main():
-    total_size = 0
-    status_codes = {'200': 0,
-                    '301': 0,
-                    '400': 0,
-                    '401': 0,
-                    '403': 0,
-                    '404': 0,
-                    '405': 0,
-                    '500': 0}
-    lines_processed = 0
+lines_processed = 0
+total_size = 0
+status_codes = {'200': 0,
+                '301': 0,
+                '400': 0,
+                '401': 0,
+                '403': 0,
+                '404': 0,
+                '405': 0,
+                '500': 0}
 
-    try:
-        for line in sys.stdin:
-            try:
-                parts = line.split()
-                file_size = int(parts[-1])
-                status_code = parts[-2]
+try:
+    for line in sys.stdin:
+        parts = line.split()
 
-                # Check if status code is valid
-                if status_code in status_codes:
-                    status_codes[status_code] += 1
-                    total_size += file_size
+        if len(parts) >= 9:
+            status_code = parts[-2]
+            file_size = int(parts[-1])
+
+            # Check if status code is valid
+            if status_code in status_codes:
+                status_codes[status_code] += 1
+                total_size += file_size
                 lines_processed += 1
 
-                # Print statistics after every 10 lines
-                if lines_processed % 10 == 0:
-                    print_stats(total_size, status_codes)
+            # Print statistics after every 10 lines
+            if lines_processed == 10:
+                print_stats(total_size, status_codes)
+                lines_processed = 0
 
-            except (ValueError, IndexError):
-                # Skip lines that do not match the specified format
-                pass
+except (ValueError, IndexError):
+    # Skip lines that do not match the specified format
+    pass
 
-    except KeyboardInterrupt:
-        # Handle keyboard interruption (CTRL + C)
-        print_stats(total_size, status_codes)
-
-
-if __name__ == "__main__":
-    main()
+except KeyboardInterrupt:
+    # Handle keyboard interruption (CTRL + C)
+    print_stats(total_size, status_codes)
