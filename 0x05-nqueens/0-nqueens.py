c3df28f418d905534591c6d40b25b39534bb3270
IdiJr <idialijnr@gmail.com>
2024-01-11 16:30:35 +0000

    N Queens

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,55 +1,121 @@
 #!/usr/bin/python3
 """ N queens puzzle solving module """
+
 import sys
 
 
+def print_board(board):
+    """ print_board
+    Args:
+        board - list of list with length sys.argv[1]
+    """
+    new_list = []
+    for i, row in enumerate(board):
+        value = []
+        for j, col in enumerate(row):
+            if col == 1:
+                value.append(i)
+                value.append(j)
+        new_list.append(value)
+
+    print(new_list)
+
+
 def is_safe(board, row, col, n):
-    # Check if there is a queen in the same row on the left
+    """ isSafe
+    Args:
+        board - list of list with length sys.argv[1]
+        row - row to check if is safe doing a movement in this position
+        col - col to check if is safe doing a movement in this position
+        n: size of the board
+    Return: True of False
+    """
+
+    # Check this row in the left side
     for i in range(col):
-        if board[i] == row or \
-           board[i] == row - (col - i) or \
-           board[i] == row + (col - i):
+        if board[row][i] == 1:
             return False
+
+    # Check upper diagonal on left side
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+
+    for i, j in zip(range(row, n, 1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+
     return True
 
 
-def solveNQUtil(board, col, n, solutions):
-    if col == n:
-        solutions.append([[i, board[i]] for i in range(n)])
-        return
+def solve_nqueens_util(board, col, number):
+    """ Auxiliar method to find the posibilities of answer
+    Args:
+        board - Board to resolve
+        col - Number of col
+        number - size of the board
+    Returns:
+        All the posibilites to solve the problem
+    """
 
-    for i in range(n):
-        if is_safe(board, i, col, n):
-            board[col] = i
-            solveNQUtil(board, col + 1, n, solutions)
+    if (col == number):
+        print_board(board)
+        return True
+    res = False
+    for i in range(number):
+
+        if (is_safe(board, i, col, number)):
+
+            # Place this queen in board[i][col]
+            board[i][col] = 1
+
+            # Make result true if any placement
+            # is possible
+            res = solve_nqueens_util(board, col + 1, number) or res
+
+            board[i][col] = 0  # BACKTRACK
+
+    return res
 
 
-def solve_nqueens(n):
-    if not isinstance(n, int):
-        print("N must be a number")
-        sys.exit(1)
+def solve_nqueens(number):
+    """ Find all the posibilities if exists
+    Args:
+        number - size of the board
+    """
+    board = [[0 for i in range(number)]for i in range(number)]
 
-    if n < 4:
-        print("N must be at least 4")
-        sys.exit(1)
+    if not solve_nqueens_util(board, 0, number):
+        return False
 
-    board = [-1] * n
-    solutions = []
+    return True
 
-    solveNQUtil(board, 0, n, solutions)
 
-    for sol in solutions:
-        print(sol)
+def validate(args):
+    """ Validate the input data to verify if the size to
+        answer is posible
+    Args:
+        args - sys.argv
+    """
+    if (len(args) == 2):
+        # Validate data
+        try:
+            number = int(args[1])
+        except Exception:
+            print("N must be a number")
+            exit(1)
+        if number < 4:
+            print("N must be at least 4")
+            exit(1)
+        return number
+    else:
+        print("Usage: nqueens N")
+        exit(1)
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: ./0-nqueens.py N")
-        sys.exit(1)
+    """ Main method to execute the application
+    """
 
-    try:
-        N = int(sys.argv[1])
-        solve_nqueens(N)
-    except ValueError:
-        print("N must be a number")
-        sys.exit(1)
+    number = validate(sys.argv)
+    solve_nqueens(number)
