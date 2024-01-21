a7740d417ad4dcbc4cf492dd7a13fd12f4f1ad80
IdiJr <idialijnr@gmail.com>
2024-01-21 06:33:05 +0000

    0x06. Star Wars API

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,45 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+if (process.argv.length !== 3) {
+  console.error('Usage: ./0-starwars_characters.js <Movie ID>');
+  process.exit(1);
+}
+
+const movieId = process.argv[2] + '/';
+const apiUrl = 'https://swapi-api.hbtn.io/api/films/';
+
+request(apiUrl + movieId, (error, response, body) => {
+  if (error) {
+    console.error('Error fetching film information:', error);
+  } else if (response.statusCode !== 200) {
+    console.error('HTTP Error fetching film information. Status Code:', response.statusCode);
+  } else {
+
+    // Parse the response body to get the list of character URLs
+    const filmData = JSON.parse(body);
+    const characters = filmData.characters;
+
+    if (characters && characters.length > 0) {
+      // Iterate through the character URLs and fetch character information
+      // Make a request to each character URL
+      characters.forEach((characterUrl) => {
+        request(characterUrl, (charError, charResponse, charBody) => {
+          if (charError) {
+            console.error('Error fetching character information:', charError);
+          } else if (charResponse.statusCode !== 200) {
+            console.error('HTTP Error fetching character information. Status Code:', charResponse.statusCode);
+          } else {
+            // Parse the character information and print the character's name
+            console.log(JSON.parse(charBody).name);
+            // You might resolve the promise here if you're using promises
+            // resolve();
+          }
+        });
+      });
+    } else {
+      console.error('No characters found for the specified movie ID.');
+    }
+  }
+});
---
+++ 0x06-starwars_api/README.md
@@ -0,0 +1,10 @@
+# 0x06. Star Wars API
+![](https://hanoverhawks.files.wordpress.com/2023/03/200956.jpg)
+This project contains the task:
+0. Star Wars Characters
+
+Write a script that prints all characters of a Star Wars movie:
+- The first positional argument passed is the Movie ID - example: 3 = “Return of the Jedi”
+- Display one character name per line in the same order as the “characters” list in the /films/ endpoint
+- You must use the Star wars API
+- You must use the request module
