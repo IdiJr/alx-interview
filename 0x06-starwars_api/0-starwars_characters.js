0d7b7f509b1b403a85b011264f1c000b6aa33ec5
IdiJr <idialijnr@gmail.com>
2024-01-21 06:35:10 +0000

    0x06. Star Wars API

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,44 +2,27 @@
 
 const request = require('request');
 
-if (process.argv.length !== 3) {
-  console.error('Usage: ./0-starwars_characters.js <Movie ID>');
-  process.exit(1);
-}
+const filmNum = process.argv[2] + '/';
+const filmURL = 'https://swapi-api.hbtn.io/api/films/';
 
-const movieId = process.argv[2] + '/';
-const apiUrl = 'https://swapi-api.hbtn.io/api/films/';
+// Makes an API request to get film information
+request(filmURL + filmNum, async function (err, res, body) {
+  if (err) return console.error(err);
 
-request(apiUrl + movieId, (error, response, body) => {
-  if (error) {
-    console.error('Error fetching film information:', error);
-  } else if (response.statusCode !== 200) {
-    console.error('HTTP Error fetching film information. Status Code:', response.statusCode);
-  } else {
+  // Parse the response body to get the list of character URLs
+  const charURLList = JSON.parse(body).characters;
 
-    // Parse the response body to get the list of character URLs
-    const filmData = JSON.parse(body);
-    const characters = filmData.characters;
+  // Iterare through the character URLs and fect character information
+  // Make a request to each character URL
+  for (const charURL of charURLList) {
+    await new Promise(function (resolve, reject) {
+      request(charURL, function (err, res, body) {
+        if (err) return console.error(err);
 
-    if (characters && characters.length > 0) {
-      // Iterate through the character URLs and fetch character information
-      // Make a request to each character URL
-      characters.forEach((characterUrl) => {
-        request(characterUrl, (charError, charResponse, charBody) => {
-          if (charError) {
-            console.error('Error fetching character information:', charError);
-          } else if (charResponse.statusCode !== 200) {
-            console.error('HTTP Error fetching character information. Status Code:', charResponse.statusCode);
-          } else {
-            // Parse the character information and print the character's name
-            console.log(JSON.parse(charBody).name);
-            // You might resolve the promise here if you're using promises
-            // resolve();
-          }
-        });
+        // Parse the charcter nformation and print the character's name Resolve the promise to indicate completion
+        console.log(JSON.parse(body).name);
+        resolve();
       });
-    } else {
-      console.error('No characters found for the specified movie ID.');
-    }
+    });
   }
 });
