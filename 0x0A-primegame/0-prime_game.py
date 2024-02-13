350ba95a1920807fa4a5445a414a2769b654852d
IdiJr <idialijnr@gmail.com>
2024-02-13 14:18:33 +0000

    modified the helper functions

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -3,18 +3,20 @@
 
 
 def is_prime(num):
-    if num <= 1:
-        return False
-    for i in range(2, int(num ** 0.5) + 1):
-        if num % i == 0:
-            return False
-    return True
+    primes = [True] * (num + 1)
+    primes[0] = primes[1] = False
+
+    for x in range(2, int(num ** 0.5) + 1):
+        if primes[x]:
+            for y in range(x ** 2, num + 1, x):
+                primes[y] = False
+    return primes
 
 
 def get_primes(n):
     primes = []
-    for i in range(2, n + 1):
-        if is_prime(i):
+    for i, is_prime_flag in enumerate(is_prime(n)):
+        if is_prime_flag:
             primes.append(i)
     return primes
 
@@ -26,19 +28,18 @@ def isWinner(x, nums):
 
     maria_wins = 0
     ben_wins = 0
-    for x in nums:
-        primes = get_primes(x)
-        # Count the number of prime numbers in the set
-        prime_count = sum(primes[2:x+1])
-        # If the count is even, Ben wins, otherwise Maria wins
-        if prime_count % 2 == 0:
-            ben_wins += 1
-        else:
-            maria_wins += 1
 
-    if maria_wins > ben_wins:
-        return "Maria"
-    elif ben_wins > maria_wins:
-        return "Ben"
-    else:
+    # generate a list of prime number based on the max numbers in num
+    n = max(nums)
+    primes = is_prime(n)
+    for n in nums:
+        # Count the number of prime numbers in the set
+        count = sum(primes[2:n + 1])
+        # If the count is even, Ben wins, otherwise Maria wins
+        ben_wins += count % 2 == 0
+        maria_wins += count % 2 == 1
+
+    if maria_wins == ben_wins:
         return None
+
+    return 'Maria' if maria_wins > ben_wins else 'Ben'
